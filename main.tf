80ed29aee8ba8b80618a2e36c5567e513cfe5094
polianaraujo16 <poliana.ellen@logap.com.br>
2025-12-09 11:52:10 -0300

    feature: main

---
+++ main.tf
@@ -0,0 +1,58 @@
+# Configurações do terraform
+# Versão do terraform, versão de providers, backends, etc
+terraform {
+    required_version = "~> 1.13.5" # 1.13.5 até 1.0.n
+
+    required_providers {
+      aws = {
+        version = "6.19.0"
+        source = "hashicorp/aws"
+      }
+    }
+
+    backend "s3" {
+      
+    }
+}
+
+
+# Definir Configurações para determinados providers que estamos criando infraestrutura
+# provider "aws" { }
+
+
+# bloco de recurso que cria e gerencia uma bucket na aws
+# resource "aws_s3_bucket" "ref_aws_se_bucket" { }
+
+
+# bloco de DataSource serve para nós irmos buscar alguma informação de algum recurso que não está sendo gerido pelo Terraform e que nós precisemos dessa informação para referenciar no nosso código ou para usar em algum momento dentro do código
+# data "aws_instance" "name" { }
+
+
+# grupo de recursos (nao referencia recursos, mas tem referência interna)
+# module "name" { }
+
+
+# variable "name_variavel" { }
+
+
+# output "name" {
+    # nao pode ter dois outputs com o mesmo nome e mesmo código }
+
+
+# locals { }
+
+
+# serve para nós importarmos determinado recurso para que o terraform gerencie aquele recurso que foi criado fora dele
+#import { }
+
+
+# mudar a referência interna que o terraform tem para determinado recurso
+# moved { }
+
+
+# nao quero que o terraform gerencie mais determinado recurso (sem destruir esse recurso)
+# removed { }
+
+
+# verificação determinada condição em determinado recurso no provider, que está sendo gerida ou não pelo terraform
+# check "name" { }
